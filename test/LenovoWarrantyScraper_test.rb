0007ed1e54f7e3969b3b659b342e625b1a943a3a
jthorpe <jthorpe@stotthoare.com.au>
2020-04-17 11:11:15 +0800

    version

--- test/LenovoWarrantyScraper_test.rb
+++ test/LenovoWarrantyScraper_test.rb
@@ -5,5 +5,10 @@ class LenovoWarrantyScraperTest < Minitest::Test
     refute_nil ::LenovoWarrantyScraper::VERSION
   end
 
+  def test_temp
+    # LenovoWarrantyScraper.single_claim(LenovoWarrantyScraper.load_secrets, LenovoWarrantyScraper.load_settings, 'R90T9B08', 'Churchlands SHS', 'S15/04/20 10:10 AM', '02HM004', 'Wont turn on', 'Disconnected battery, reseated ram. tested spare ram. tested spare motherboard working' )
+    LenovoWarrantyScraper.single_claim(LenovoWarrantyScraper.load_secrets, LenovoWarrantyScraper.load_settings, 'R90SFGM6', 'Churchlands SHS', 'S15/04/20 10:10 AM', '01YP240', 'Keys on keyboard not working', 'Reseated keyboard, updated bios, reimaged device, tested spare keyboard working' )
+
+  end
 
 end
