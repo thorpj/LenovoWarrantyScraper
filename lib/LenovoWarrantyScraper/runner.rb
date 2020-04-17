ec3200f6611179d10a53a089cf9a4c4bfabb737c
jthorpe <jthorpe@stotthoare.com.au>
2020-04-17 11:31:49 +0800

    m

--- lib/LenovoWarrantyScraper/runner.rb
+++ lib/LenovoWarrantyScraper/runner.rb
@@ -22,7 +22,7 @@ module LenovoWarrantyScraper
         @scraper = LenovoWarrantyScraper::Scraper.new(@secrets, @settings)
         warranty_reference = @scraper.make_adp_clw_claim(serial_number, parts, ticket_number, failure_description, comments, account, service_type)
         warranty_reference
-      rescue Selenium::WebDriver::Error::NoSuchElementError, Selenium::WebDriver::Error::ObsoleteElementError, Selenium::WebDriver::Error::UnhandledError, Selenium::WebDriver::Error::ExpectedError, Selenium::WebDriver::Error::NoSuchWindowError, Selenium::WebDriver::Error::InvalidSessionIdError, StandardError => e
+      rescue Selenium::WebDriver::Error::NoSuchElementError, Selenium::WebDriver::Error::StaleElementReferenceError, Selenium::WebDriver::Error::UnknownError, Selenium::WebDriver::Error::ExpectedError, Selenium::WebDriver::Error::NoSuchWindowError, Selenium::WebDriver::Error::InvalidSessionIdError, StandardError => e
         $logger.debug e.backtrace
         @scraper.quit if @scraper.respond_to? :quit
         raise ApiError.new "Claim Failed: #{e.inspect} #{e.message} #{{serial_number: serial_number, parts: parts, ticket_number: ticket_number, failure_description: failure_description, comments: comments, account: account}}"
